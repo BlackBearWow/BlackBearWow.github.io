62a3af857858170ad33f063aa3b1b1ab409c4903
BlackBearWow <pat0407@naver.com>
2021-09-15 02:54:09 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function f() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", "숫자야구.html", true);
-  xhttp.send();
-}
+alert("Loaded script.js"); 
